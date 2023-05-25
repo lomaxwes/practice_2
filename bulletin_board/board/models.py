80a68c34340e154382721122b027537ead7e50d1
lomaxwes <lomaxwes@gmail.com>
2023-05-25 13:59:47 +0500

    добавлена модель Post

--- bulletin_board/board/models.py
+++ bulletin_board/board/models.py
@@ -1,3 +1,25 @@
+from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    CATEGORY_CHOICES = [
+        ('Tanks', 'Танки'),
+        ('Healers', 'Хилы'),
+        ('DD', 'ДД'),
+        ('Merchants', 'Торговцы'),
+        ('Guild Masters', 'Гилдмастеры'),
+        ('Quest Givers', 'Квестгиверы'),
+        ('Blacksmiths', 'Кузнецы'),
+        ('Tanners', 'Кожевники'),
+        ('Potion Makers', 'Зельевары'),
+        ('Spell Masters', 'Мастера заклинаний')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=254)
+    comment = RichTextField()
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, )
+    creation_time = models.DateTimeField(auto_now_add=True)
