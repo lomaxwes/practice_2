e037a13b0171ee666f70f5fe7972d9692bad2db0
lomaxwes <lomaxwes@gmail.com>
2023-05-25 14:35:57 +0500

    добавлены шаблоны URL для представлений, связанных с аккаунтом

--- bulletin_board/account/forms.py
+++ bulletin_board/account/forms.py
@@ -4,10 +4,21 @@ from django.contrib.auth.models import User
 
 class BaseRegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Пароли не совпадают.")
+
+        return cleaned_data
 
 
 class LoginForm(forms.Form):
