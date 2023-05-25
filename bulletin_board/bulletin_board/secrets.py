f3d24eedc053459b7ee3881d9b50bf19e75546c5
lomaxwes <lomaxwes@gmail.com>
2023-05-25 13:52:20 +0500

    initial commit

---
+++ bulletin_board/bulletin_board/secrets.py
@@ -0,0 +1,1 @@
+DJN_KEY = 'django-insecure-qc=ea49$yp59py#k1911^%9fu3l%mcv!d41k2&%p3v+-^i-h3r'
--- bulletin_board/bulletin_board/settings.py
+++ bulletin_board/bulletin_board/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from .secrets import DJN_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qc=ea49$yp59py#k1911^%9fu3l%mcv!d41k2&%p3v+-^i-h3r'
+SECRET_KEY = DJN_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'board',
 ]
 
 MIDDLEWARE = [
