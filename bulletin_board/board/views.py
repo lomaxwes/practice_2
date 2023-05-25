1ecd3fcae1a2b3d97e0d37039ac9ed6af9b60566
lomaxwes <lomaxwes@gmail.com>
2023-05-25 15:21:09 +0500

    добавлены представления

--- bulletin_board/board/views.py
+++ bulletin_board/board/views.py
@@ -1,3 +1,115 @@
-from django.shortcuts import render
+from django.views import generic
+from django.shortcuts import HttpResponseRedirect, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.core.mail import send_mail
+from .models import Post, User, Response
+from .forms import PostCreateForm, ResponseCreateForm
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    model = Post
+    ordering = '-creation_time'
+    template_name = 'board/post_list.html'
+    context_object_name = 'post_list'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'board/post_detail.html'
+
+
+class PostCreate(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    template_name = 'board/post_edit.html'
+    form_class = PostCreateForm
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        result = super().form_valid(form)
+        return result
+
+
+class PostUpdate(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    form_class = PostCreateForm
+    template_name_suffix = '_edit'
+
+    def get_template_names(self):
+        post = self.get_object()
+        if post.user != self.request.user:
+            raise PermissionDenied
+        return super().get_template_names()
+
+
+class ResponseList(LoginRequiredMixin, generic.ListView):
+    model = Response
+    template_name = 'board/response_list.html'
+    context_object_name = 'response_list'
+    ordering = '-creation_time'
+
+    def get_queryset(self):
+        queryset = Response.objects.filter(post__user=self.request.user)
+        return queryset
+
+
+class ResponseDetail(LoginRequiredMixin, generic.DetailView):
+    model = Response
+    template_name_suffix = '_detail'
+
+    def get_template_names(self):
+        response = self.get_object()
+        if response.post.user != self.request.user:
+            raise PermissionDenied
+        return super().get_template_names()
+
+
+class ResponseCreate(LoginRequiredMixin, generic.CreateView):
+    model = Response
+    template_name = 'board/response_create.html'
+    form_class = ResponseCreateForm
+    success_url = '/success/'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.post = Post.objects.get(id=self.kwargs['pk'])
+        self.object.save()
+        result = super().form_valid(form)
+        send_mail(
+            subject=f'Получен отклик на пост "{self.object.post.title}"',
+            message=f'Отклик: "{self.object.comment}"',
+            from_email='aidigo.grigorjev@yandex.ru',
+            recipient_list=[self.object.post.user.email]
+        )
+        return result
+
+
+class SuccessView(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'board/success.html'
+
+
+@login_required
+def accept_response(request, pk):
+    response = Response.objects.get(pk=pk)
+    response.status = 'Y'
+    response.save()
+    send_mail(
+        subject=f'Доска объявлений: отклик принят',
+        message=f'Ваш отклик на пост "{response.post.title}" принят',
+        from_email='aidigo.grigorjev@yandex.ru',
+        recipient_list=[response.user.email]
+    )
+    return HttpResponseRedirect(reverse('response_list'))
+
+
+@login_required
+def deny_response(request, pk):
+    response = Response.objects.get(pk=pk)
+    response.status = 'N'
+    response.save()
+    return HttpResponseRedirect(reverse('response_list'))
+
